e3fb7e79309c7f5513d6422efe5094ecc17b11f0
eden0521 <a8505212005@gmail.com>
2021-03-23 14:33:04 +0800

    Update westernT50.js

--- music/westernT50.js
+++ music/westernT50.js
@@ -5,7 +5,7 @@ const app = new APlayer({
     theme: '#1c03fc',
     loop: 'all',
     order: 'random',
-    preload: 'metadata',
+    preload: 'none',
     volume: 0.7,
     mutex: true,
     listFolded: false,
